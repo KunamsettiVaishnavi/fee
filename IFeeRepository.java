727c4f67d2550941e5306b5c6f914a2a0ae3e493
KunamsettiVaishnavi <71434509+KunamsettiVaishnavi@users.noreply.github.com>
2021-06-06 12:55:54 +0530

    Add files via upload

---
+++ IFeeRepository.java
@@ -0,0 +1,21 @@
+package com.g3.spc.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.g3.spc.entities.Fee;
+import com.g3.spc.entities.Student;
+@Repository
+public interface IFeeRepository extends JpaRepository<Fee, Integer> {
+	/*
+		public Fee addFee(Fee fee);
+		public Fee deleteFee(int id);
+		public Fee updateFee(Fee fee);
+		public Fee retrieveFee(int id);
+		public Fee retrieveFeeByStudent(Student student);
+		public List<Fee> retrieveAllFeeByMonth(int month);
+		
+		*/
+}
